4f4a92461711668503e2d855f8cec7ac813e272e
abuabdelrahman1413 <ms22abuabdelrahman@gmail.com>
2024-08-17 10:03:49 +0200

    input_output: 2

---
+++ input_output/2.py
@@ -0,0 +1,8 @@
+# greeter.py
+
+# Ask the user for their name
+first_name = input("What is your name? ")
+last_name = input("What is your name? ")
+name = first_name + " " + last_name
+# Output the greeting
+print(f"Hello, {name}!")
