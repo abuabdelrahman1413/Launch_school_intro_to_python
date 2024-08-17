ea9a6cd52a66e9dea6edf2dc7e05c8c0f755921f
abuabdelrahman1413 <ms22abuabdelrahman@gmail.com>
2024-08-17 11:28:26 +0200

    flow_control: 5

---
+++ flow_control/5.py
@@ -0,0 +1,9 @@
+def is_list_empty(my_list):
+    if my_list:
+        print('Not Empty')
+    else:
+        print('Empty')
+
+is_list_empty([])
+
+# output: Empty because the list is empty, empty list is falsy
