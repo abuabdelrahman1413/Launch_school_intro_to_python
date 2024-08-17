f2f2c397faf06411061f9f594aa897655a6a5242
abuabdelrahman1413 <ms22abuabdelrahman@gmail.com>
2024-08-17 10:59:31 +0200

    functions: 18,19

---
+++ functions/19.py
@@ -0,0 +1,13 @@
+def remainders_3(numbers):
+    return [number % 3 for number in numbers]
+
+
+numbers_1 = [0, 1, 2, 3, 4, 5, 6]
+numbers_2 = [1, 2, 4, 5]
+numbers_3 = [0, 3, 6]
+numbers_4 = []
+
+print(all(remainders_3(numbers_1)))     # True
+print(all(remainders_3(numbers_2)))     # True
+print(all(remainders_3(numbers_3)))     # False
+print(all(remainders_3(numbers_4)))     # False
