b626389ffb7b7678d9376113e7d92bffc45966d2
abuabdelrahman1413 <ms22abuabdelrahman@gmail.com>
2024-08-17 09:53:34 +0200

    variables: 7

---
+++ variables/7.py
@@ -0,0 +1,12 @@
+NAME = 'Victor'
+print('Good Morning, ' + NAME)
+print('Good Afternoon, ' + NAME)
+print('Good Evening, ' + NAME)
+
+NAME = 'Nina'
+print('Good Morning, ' + NAME)
+print('Good Afternoon, ' + NAME)
+print('Good Evening, ' + NAME)
+
+# first print Victor, second print Nina
+# The value of NAME is changed to 'Nina' in the second half of the code.
