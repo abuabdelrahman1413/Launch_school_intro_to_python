cd592d338e1d45edc30c44fe3a5914439bf61e35
abuabdelrahman1413 <ms22abuabdelrahman@gmail.com>
2024-08-17 08:17:50 +0200

    using_python: 1

---
+++ using_python/1.py
@@ -0,0 +1,12 @@
+$ mkdir my_folder
+$ cd my_folder
+$ touch one.py
+$ touch two.py
+
+# Use your editor to edit one.py and two.py
+
+$ python one.py
+This is file one
+
+$ python two.py
+This is file two
