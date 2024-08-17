aa350f62b93457df5ac7afe47f992acbcefafc73
abuabdelrahman1413 <ms22abuabdelrahman@gmail.com>
2024-08-17 09:35:54 +0200

    variables: 1

---
+++ variables/1.py
@@ -0,0 +1,8 @@
+# index == idiomatic
+# CatName == idiomatic
+# lazy_dog == idiomatic
+# quick_Fox == non_idiomatic == quick_fox
+# 1stCharacter == non_ligal == first_char 
+# operand2 == idiomatic
+# BIG_NUMBER == idiomatic
+# π == illegal
