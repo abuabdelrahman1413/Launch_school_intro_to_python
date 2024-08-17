8f4d727cf8c493ed7aaf125d048a87dd919a888a
abuabdelrahman1413 <ms22abuabdelrahman@gmail.com>
2024-08-17 18:06:16 +0200

    loops: 6

---
+++ Loops_and_Iterating/6.py
@@ -0,0 +1,12 @@
+my_list = [
+    [1, 3, 6, 11],
+    [4, 2, 4],
+    [9, 17, 16, 0],
+]
+new_list = []
+for i in my_list:
+    for j in i:
+        if j % 2 == 0:
+            new_list.append(j)
+
+print(new_list)
