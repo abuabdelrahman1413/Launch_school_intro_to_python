1e470e7254a3f0be45ea2bd673772e0bbb37664a
abuabdelrahman1413 <ms22abuabdelrahman@gmail.com>
2024-08-18 18:00:58 +0200

    vars_as_pointers: 1

---
+++ Variables_as_Pointers/1.py
@@ -0,0 +1,8 @@
+# In your own words, explain the difference between these two expressions.
+my_object1 = "hello"
+my_object2 = "hello"
+
+my_object1 == my_object2
+my_object1 is my_object2
+
+# Answer: The first expression checks if the values of the two objects are equal, while the second expression checks if the two objects are the same object in memory.
